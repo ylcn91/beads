dfdfc5e11b67d717a2b36167bf4ea68cfa4e3e6b
Yalçın Doksanbir <yalcindoksanbir@gmail.com>
2026-05-28 22:34:01 +0300

    honor BEADS_DOLT_SERVER_TLS during init
    Cherry-pick of upstream PR gastownhall/beads#3679 (feature/support-tls-in-init).

--- cmd/bd/init.go
+++ cmd/bd/init.go
@@ -868,6 +868,7 @@ Non-interactive mode (--non-interactive or BD_NON_INTERACTIVE=1):
 			ProxiedServer:   initProxiedServer,
 			CreateIfMissing: true, // bd init is the only path that should create databases
 			AutoStart:       initServerMode && os.Getenv("BEADS_DOLT_AUTO_START") != "0",
+			ServerTLS:       os.Getenv("BEADS_DOLT_SERVER_TLS") != "0",
 		}
 		if serverHost != "" {
 			doltCfg.ServerHost = serverHost
